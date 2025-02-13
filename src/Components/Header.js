e8ec1326d8d248a6c24334bba22aa26cac3893d7
DannySoundarajD <soundarajdanny@gmail.com>
2025-02-14 02:59:44 +0530

    Updates

--- src/Components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './Header.css'
-function Header(){
-    return(
-        <h1 className="bigil">Welcome to the Weather Application</h1>
-    )
-}
-
-export default Header;
